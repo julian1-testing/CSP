d33a1cc4c8f8518fe04f9f00c4bd1aaae90afcab
julian1 <julian.atkinson71@gmail.com>
2017-03-24 11:39:28 +1100

    update

--- main.hs
+++ main.hs
@@ -1,18 +1,55 @@
-module Main where
+
+module Main
+where
  
-import System.Environment
+import Text.XML.HXT.Core
+-- import Text.XML.HXT....   -- further HXT packages
 import Text.XML.HXT.Core
 import Text.XML.HXT.Curl
  
+import System.IO
+import System.Environment
+import System.Console.GetOpt
+import System.Exit
+
+
+
+ 
 main :: IO ()
-main = do
-  [src, dst] <- getArgs
-  runX $
-    readDocument [withValidate no
-                 ,withCurl []
-                 ] src
-    >>>
-    writeDocument [withIndent yes
-                  ,withOutputEncoding isoLatin1
-                  ] dst
-  return ()
+main
+    = do
+      argv <- getArgs
+      (al, src, dst) <- cmdlineOpts argv
+      [rc]  <- runX (application al src dst)
+      if rc >= c_err
+      then exitWith (ExitFailure (0-1))
+      else exitWith ExitSuccess
+     
+-- | the dummy for the boring stuff of option evaluation,
+-- usually done with 'System.Console.GetOpt'
+ 
+cmdlineOpts   :: [String] -> IO (SysConfigList, String, String)
+cmdlineOpts argv
+    = return ([withValidate no], argv!!0, argv!!1)
+ 
+-- | the main arrow
+ 
+application :: SysConfigList -> String -> String -> IOSArrow b Int
+application cfg src dst
+    = configSysVars cfg                                           -- (0)
+      >>>
+      readDocument [] src
+      >>>
+      processChildren (processDocumentRootElement `when` isElem)  -- (1)
+      >>>
+      writeDocument [] dst                                        -- (3)
+      >>>
+      getErrStatus
+ 
+ 
+-- | the dummy for the real processing: the identity filter
+ 
+processDocumentRootElement  :: IOSArrow XmlTree XmlTree
+processDocumentRootElement 
+    =  this -- substitute this by the real application
+
