6e3d24ab1708ae3ae5d83ba8b92e1dd5a0373d50
julian1 <julian.atkinson71@gmail.com>
2017-03-28 15:20:57 +1100

    tidy

--- client.hs
+++ client.hs
@@ -1,36 +1,28 @@
 -- stack --install-ghc --resolver lts-5.13 runghc --package http-conduit
 
-
-{-# LANGUAGE OverloadedStrings #-}
-
-
+-- {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Arrows, NoMonomorphismRestriction #-}
+
 import Text.XML.HXT.Core
-
-
 
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types.Status (statusCode)
 
---
--- import Data.ByteString.Lazy.Char8 as Char8
-import Data.ByteString.Lazy.Char8 (unpack)
+import Data.ByteString.Lazy.Char8(unpack)
 
--- import Network.HTTP.Client.Internal(ResponseTimeout)
 
 
 parseXML s = readString [ withValidate no
     , withRemoveWS yes  -- throw away formating WS
-    ] s 
+    ] s
 
 
 atTag tag = deep (isElem >>> hasName tag)
- 
- 
 
 
-getTeams1 = atTag "gmd:CI_OnlineResource" >>>
+
+getOnlineResources1 = atTag "gmd:CI_OnlineResource" >>>
   proc l -> do
     -- leagName <- getAttrValue "NAME"   -< l
     protocol <- atTag "gmd:protocol" >>> getChildren >>> hasName "gco:CharacterString" >>> getChildren >>> getText -< l
@@ -39,16 +31,14 @@ getTeams1 = atTag "gmd:CI_OnlineResource" >>>
 
 
 
- 
-
 main :: IO ()
 main = do
-    let settings = tlsManagerSettings  { managerResponseTimeout = responseTimeoutMicro $ 60 * 1000000 } 
-    manager <- newManager settings 
+    let settings = tlsManagerSettings  { managerResponseTimeout = responseTimeoutMicro $ 60 * 1000000 }
+    manager <- newManager settings
 
 
-    let url = "https://catalogue-portal.aodn.org.au/geonetwork/srv/eng/csw?request=GetRecordById&service=CSW&version=2.0.2&elementSetName=full&id=4402cb50-e20a-44ee-93e6-4728259250d2&outputSchema=http://www.isotc211.org/2005/gmd" 
-    request <- parseRequest url -- "http://httpbin.org/post"
+    let url = "https://catalogue-portal.aodn.org.au/geonetwork/srv/eng/csw?request=GetRecordById&service=CSW&version=2.0.2&elementSetName=full&id=4402cb50-e20a-44ee-93e6-4728259250d2&outputSchema=http://www.isotc211.org/2005/gmd"
+    request <- parseRequest url
 
     response <- httpLbs request manager
 
@@ -57,10 +47,10 @@ main = do
     -- convert internal.ByteString to String
     let s = unpack $ responseBody response
 
-    teams <- runX (parseXML s  >>> getTeams1)
+    teams <- runX (parseXML s  >>> getOnlineResources1)
     let lst = Prelude.map (\(a,b) -> " ->" ++ a ++ " ->" ++ b ) teams
     mapM print lst
 
-    
+
     print "done"
- 
+
