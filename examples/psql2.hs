ee47e52454211aa25e9d7238cda2881db25a624e
julian1 <julian.atkinson71@gmail.com>
2017-03-29 15:05:55 +1100

    got sql insert

--- examples/psql2.hs
+++ examples/psql2.hs
@@ -10,25 +10,18 @@ main = hello
 
 -- hello :: IO Int
 hello = do
-  -- conn <- connectPostgreSQL "host='dbprod.emii.org.au' dbname='harvest' user='jfca' sslmode='require'"
-  -- [Only i] <- query_ conn "select 2 + 2"
 
   conn <- connectPostgreSQL "host='postgres.localnet' dbname='harvest' user='harvest' sslmode='require'"
-  -- [ Only i, j ] <- query_ conn "select id,url from catalog"
-  -- [ (i, j) ] <- query_ conn "select id,url from catalog"
-  -- xs <- query_ conn "select id,url from catalog"
-  -- print (xs :: [Only Int])
-
-  -- [Only i :: Integer] <- query_ conn "select id from catalog"
-  -- xs :: [ (Int, Int) ] <- query_ conn "select id, url from catalog"
-  -- print (xs :: Only Int  ::  Only String ])
 
   xs :: [ (Int, String) ] <- query_ conn "select id, url from catalog"
-
 
   let formatRow (id,url) = foldr (++) "" [ show id, " ",  url ]
 
   mapM  (putStrLn.formatRow)  xs
+
+
+  -- Note also executeMany 
+  execute conn "insert into catalog(url) values (?)" [("catalog3" :: String ) ]
 
   return ()
 
@@ -37,4 +30,14 @@ hello = do
   -- mapM  (\a -> putStrLn $ formatRow a ) xs
   -- mapM (\(id,url) -> putStrLn $ show id ++ ", " ++ url) xs
 
+  -- [ Only i, j ] <- query_ conn "select id,url from catalog"
+  -- [ (i, j) ] <- query_ conn "select id,url from catalog"
+  -- xs <- query_ conn "select id,url from catalog"
+  -- print (xs :: [Only Int])
+
+  -- [Only i :: Integer] <- query_ conn "select id from catalog"
+  -- xs :: [ (Int, Int) ] <- query_ conn "select id, url from catalog"
+  -- print (xs :: Only Int  ::  Only String ])
+  -- conn <- connectPostgreSQL "host='dbprod.emii.org.au' dbname='harvest' user='jfca' sslmode='require'"
+  -- [Only i] <- query_ conn "select 2 + 2"
 
