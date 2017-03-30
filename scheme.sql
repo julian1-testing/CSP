587cde1a1720a7e4f46c9c2ea23853608738c172
julian1 <julian.atkinson71@gmail.com>
2017-03-31 09:00:23 +1100

    got view with correct narrower and narrow_match

--- scheme.sql
+++ scheme.sql
@@ -1,10 +1,19 @@
 
 
-select * from concept 
+select 
+  concept.label,
+  narrower_concept.label,
+  narrow_match_concept.label
+
+  from concept 
   left join narrower on     concept.id = narrower.narrower_id 
   left join narrow_match on concept.id = narrow_match.narrower_id 
 
-  order by url
+
+  left join concept as narrower_concept on narrower_concept.id = narrower.concept_id
+  left join concept as narrow_match_concept on narrow_match_concept.id = narrow_match.concept_id
+
+  order by concept.url
 
 ;
 
