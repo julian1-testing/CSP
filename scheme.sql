392062cafbffdb4514fb69c66ede6e136ed72c52
julian1 <julian.atkinson71@gmail.com>
2017-03-31 11:18:28 +1100

    ok got scheme

--- scheme.sql
+++ scheme.sql
@@ -3,15 +3,20 @@
 select 
   concept.label,
   narrower_concept.label as narrower_label,
-  narrow_match_concept.label as narrow_match_label
+  narrow_match_concept.label as narrow_match_label,
+  scheme.title 
 
   from concept 
   left join narrower on     concept.id = narrower.narrower_id 
   left join narrow_match on concept.id = narrow_match.narrower_id 
+  left join in_scheme on    concept.id = in_scheme.concept_id 
 
 
   left join concept as narrower_concept on narrower_concept.id = narrower.concept_id
   left join concept as narrow_match_concept on narrow_match_concept.id = narrow_match.concept_id
+
+
+  left join scheme on scheme.id = in_scheme.scheme_id
 
   order by concept.url
 
