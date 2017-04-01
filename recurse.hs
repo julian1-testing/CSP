fd6605994ba7b0e68de1c24930e0155774f1b573
julian1 <julian.atkinson71@gmail.com>
2017-04-01 15:01:49 +1100

    ok working - but how fast

--- recurse.hs
+++ recurse.hs
@@ -7,57 +7,28 @@
 
 {-# LANGUAGE QuasiQuotes #-}
 
--- import Text.XML.HXT.Core
-
-{-
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as BC
-import qualified Data.ByteString.Lazy.Char8 as BLC
--}
 
 import Database.PostgreSQL.Simple
 
 import Text.RawString.QQ
 
 
--- do we want the id
--- should carry a recursion depth as well 
--- to make it easy to print...
--- think we should pass 
-
-{-
-    case length xs of
-      1 -> do
-        -- store the concept
-        let (concept_id, concept_label) : _ = xs
-        execute conn [r|
--}
- 
-
-pad s count = 
-  case count == 0 of 
+pad s count =
+  case count == 0 of
     True -> s
     False -> pad (" " ++ s) (count - 1)
 
 
-
-recurse conn depth (parent_id, label) = do 
-
-  putStrLn $ (pad "" $ depth * 3) ++  "doing id "  ++ show (parent_id, label)
+recurse conn depth (parent_id, label) = do
+  putStrLn $ (pad "" $ depth * 3) ++ "- " ++ show parent_id ++ " " ++ label
 
   let query1 = [r|
-        select id, label 
-        from concept_view  
+        select id, label
+        from concept_view
         where parent_id = ?
   |]
-
   xs :: [ (Integer, String) ] <- query conn query1 (Only parent_id)
-
-  -- mapM (recurse conn) xs
-  -- mapM print  xs
-
-  mapM (\(a,b) -> recurse  conn (depth + 1 :: Integer) (a,b) )  xs
-
+  mapM (recurse conn $ depth + 1) xs
   return ()
 
 
@@ -68,9 +39,9 @@ main = do
   conn <- connectPostgreSQL "host='postgres.localnet' dbname='harvest' user='harvest' sslmode='require'"
 
   let query1 = [r|
-        select id, label 
-        from concept_view  
-        where parent_id is null 
+        select id, label
+        from concept_view
+        where parent_id is null
         and scheme_title ~ ?
   |]
 
@@ -79,12 +50,7 @@ main = do
 
   xs :: [ (Integer, String) ] <- query conn query1 (Only url)
 
-  -- mapM print xs
-  -- mapM (\(id,label) -> recurse conn id) xs
   mapM (recurse conn 0) xs
-
-  -- now we want to recurse... and propagate up.... 
-  -- hmmmm it's a bit messy to get the counts.
 
   return ()
 
