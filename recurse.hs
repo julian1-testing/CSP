d01060d3252978d121df5b328fd4bd09fe862b3c
julian1 <julian.atkinson71@gmail.com>
2017-04-01 14:42:12 +1100

    some recursion

--- recurse.hs
+++ recurse.hs
@@ -20,11 +20,31 @@ import Database.PostgreSQL.Simple
 import Text.RawString.QQ
 
 
+-- do we want the id
+-- should carry a recursion depth as well 
+-- to make it easy to print...
+-- think we should pass 
+
+recurse conn (parent_id, label) = do 
+
+  print $ "doing id "  ++ show parent_id
+
+  let query1 = [r|
+        select id, label 
+        from concept_view  
+        where parent_id = ?
+  |]
+
+  xs :: [ (Integer, String) ] <- query conn query1 (Only parent_id)
+
+  mapM print xs
+
+
+
 main :: IO ()
 main = do
 
   conn <- connectPostgreSQL "host='postgres.localnet' dbname='harvest' user='harvest' sslmode='require'"
-
 
   let query1 = [r|
         select id, label 
@@ -38,6 +58,11 @@ main = do
   xs :: [ (Integer, String) ] <- query conn query1 (Only url)
 
   mapM print xs
+  -- mapM (\(id,label) -> recurse conn id) xs
+  mapM (recurse conn ) xs
+
+  -- now we want to recurse... and propagate up.... 
+  -- hmmmm it's a bit messy to get the counts.
 
   return ()
 
