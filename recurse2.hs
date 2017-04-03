f262a1567a20e4a9c75a3e6e0b1bb5d2522c7fef
julian1 <julian.atkinson71@gmail.com>
2017-04-04 08:16:21 +1000

    update

--- recurse2.hs
+++ recurse2.hs
@@ -14,14 +14,14 @@ import Control.Arrow ((>>>), (<<<))
 import Data.Function( (&) )
 
 
-import Data.Set(toList, fromList) 
+import Data.Set(toList, fromList)
 
 import Debug.Trace(trace)
 
 
 {-
   - remember it's not a tree - and we cannot necessarily easily recurse.
-  - instead sweep through as flat lists - and move the items to their parents.  
+  - instead sweep through as flat lists - and move the items to their parents.
   - can be a list that we de-duplicate.... or another map
   - until we get to the top node.
 
@@ -35,18 +35,18 @@ mkUniq = toList . fromList
 
 
 -- ease syntax
-mapGet m e = 
-  -- trace  ("mytrace - mapGet e: " ++ show e ++ " m: " ++ show m) $ 
+mapGet m e =
+  -- trace  ("mytrace - mapGet e: " ++ show e ++ " m: " ++ show m) $
   (Map.!) m e
 
 
 
 getConceptNesting conn  = do
   -- get parent child concept nesting
-  -- we want the concept_id, parent_id, record_id 
+  -- we want the concept_id, parent_id, record_id
   let query1 = [r|
-      select 
-        id as concept_id, 
+      select
+        id as concept_id,
         parent_id
       from concept_view ;
   |]
@@ -55,17 +55,17 @@ getConceptNesting conn  = do
 
 
 
--- doesn't have a null parent_id because - it's only the terms that actually appear in the record 
+-- doesn't have a null parent_id because - it's only the terms that actually appear in the record
 
 getFacetList conn  = do
-  -- we want the concept_id, parent_id, record_id 
+  -- we want the concept_id, parent_id, record_id
   let query1 = [r|
-      select 
-        facet.concept_id, 
+      select
+        facet.concept_id,
         concept_view.parent_id,  -- parent concept
         facet.record_id           -- the record
-      from facet 
-      left join concept_view on concept_view.id = facet.concept_id  
+      from facet
+      left join concept_view on concept_view.id = facet.concept_id
       order by concept_id
       -- where concept_id = 576 ;
   |]
@@ -89,125 +89,95 @@ buildFacetMap xs =
 
   where
     --  insert an empty list for concept_id
-    conceptEmpty m (concept_id, _, _) = 
+    conceptEmpty m (concept_id, _, _) =
       Map.insert (Just concept_id) [] m
 
 --    -- the same - except for parent_id
---    f' m (_, parent_id, _) = 
+--    f' m (_, parent_id, _) =
 --      Map.insert parent_id [] m
 --      m
-   
-    -- TODO - maybe don't populate everything - not sure... - decide later - 
-    
+-- TODO - maybe don't populate everything - not sure... - decide later -
+
     -- populate concept list with the records
     f m (concept_id, _, record_id) =
       let current = mapGet m (Just concept_id) in
-      let new = record_id : current in 
-      Map.insert (Just concept_id) new m 
+      let new = record_id : current in
+      Map.insert (Just concept_id) new m
 
 
 
 propagateToParent m nesting =
-  -- change name to propgateUp
-  -- fold over the concept/parent relationships and push the record_id into their parent concept
+  -- fold over the concept/parent nesting relationships and push the record_id's up into their parent concept
 
-  trace  "trace RRRRRR" $
-  foldl f Map.empty nesting 
-
+  foldl f Map.empty nesting
   where
-    --  insert an empty list for concept_id
-    f newMap (concept_id, parent_id) =  
-      trace  ("trace relationship -> " ++ show (concept_id, parent_id)) $
-
-      -- hang on.... are destructuring here? here ...
-      -- it's not a member?  why not?
+    f newMap (concept_id, parent_id) =
+      -- trace  ("trace nesting -> " ++ show (concept_id, parent_id)) $
 
       case Map.member (Just concept_id) m of
         True ->
-          -- this is weird - why isn't the parent of Nothing in there....
-          trace  ("trace here2 -> " ++ show (concept_id, parent_id)) $
+          -- trace  ("trace here2 -> " ++ show (concept_id, parent_id)) $
 
           -- get the set for this concept
           let newSet = mapGet m (Just concept_id) in
           -- get the list of records for the parent - might be top level - Nothing
-          let currentParentLst = ( 
+          let currentParentLst = (
                 case Map.member parent_id newMap of
                   False -> []
-                  True -> mapGet newMap parent_id 
-                ) 
+                  True -> mapGet newMap parent_id
+                )
           in
           -- combine them
-          let newParentLst  = mkUniq (currentParentLst ++ newSet)  in 
+          let newParentLst  = mkUniq (currentParentLst ++ newSet)  in
           -- and store in terms of the parent_id
           Map.insert parent_id newParentLst newMap
 
         -- ignore - if no records associated with this concept
-        False -> 
+        False ->
           newMap
 
-  
 
-printMap m = do 
+
+printMap m = do
     m
-    & Map.toList 
+    & Map.toList
     & map (\(concept_id, xs) -> (concept_id, length xs, xs)) -- add length
-    & mapM print 
- 
+    & mapM print
+
 
 
 main :: IO ()
 main = do
   conn <- connectPostgreSQL "host='postgres.localnet' dbname='harvest' user='harvest' sslmode='require'"
 
-  print "nesting"
-  nesting <- getConceptNesting conn 
-  mapM print nesting 
+  nesting <- getConceptNesting conn
+  -- print "nesting"
+  -- mapM print nesting
 
-  -- ok, we have the link with nothing... 
-
-  -- there is no concept with id nothing ... -- ahhh but we can remove the item.... 
-
-  -- but there may be in the map
-
-  -- TODO change name of table to facetIndex or facet - postgres join table name
-  -- conceptRecord --- as the assocation.... or conceptRecordMap
-  -- get the facet associations from 
+  -- get the facet concept and record associations from the db
   facetList <- getFacetList conn
   -- print "facet list"
   -- mapM print $ facetList
 
 
-
   print "######################## 0"
-  -- build mapping from concept -> records
   let m = buildFacetMap facetList
   printMap m
 
   print "######################## 1"
-  let m'  = propagateToParent m nesting 
+  let m'  = propagateToParent m nesting
   printMap m'
 
 
   print "######################## 2"
-  let m''  = propagateToParent m' nesting 
+  let m''  = propagateToParent m' nesting
   printMap m''
 
   print "######################## 3"
-  let m'''  = propagateToParent m'' nesting 
+  let m'''  = propagateToParent m'' nesting
   printMap m'''
-{-
-  print "######################## 4"
-  let m''  = propagateToParent m' nesting 
-  printMap m''
-
--}
-
-  -- ok, i think it doesn't propagate up, because we don't have it in the nesting....
-
 
   return ()
-
-
 
 
 
@@ -220,7 +190,7 @@ main = do
   -}
 
   -- (\m -> foldl f2 m' xs ).  (\m -> foldl f' m xs ) . (\m -> foldl f m xs )  Map.empty
-{- 
+{-
   (\m' -> foldl f2 m' xs) $  (\m -> foldl f' m xs) $  (foldl f Map.empty xs)
 -}
   -- propagate the records into the next level up...
@@ -230,25 +200,25 @@ main = do
   -- TODO make sure they are unique...
 
   -- VERY IMPORTANT - the XS set will have to be generated from the current list
-  -- let xs = Map. 
+  -- let xs = Map.
   -- this is not so easy....
   -- let xs = Map.toList m in
 
   -- ok, it's more complicated - because we need concept_id
-  -- ok converting to 
+  -- ok converting to
 
-  -- HANG on. If we are not using the list.... 
-  -- NO. it may be ok. we just loop through everythign to 
+  -- HANG on. If we are not using the list....
+  -- NO. it may be ok. we just loop through everythign to
 
- 
+
 {-
   case count == 0 of
     True -> s
     False -> pad' (" " ++ s) (count - 1)
 
 
-  -- only now we want to propagate everything to the parent 
-  -- and perhaps 
+  -- only now we want to propagate everything to the parent
+  -- and perhaps
 
   -- issue in propagating up --- is that we may propagate more than once....
   -- depending on the ordering....
@@ -256,15 +226,15 @@ main = do
   -- holy hell....
 
   -- is there not a simpler way to do this...
- 
-  -- IMPORTANT... 
+
+  -- IMPORTANT...
   -- create a new map ...
 
 -}
 
 {-
       case length  newParentLst of
-        0 -> newMap 
+        0 -> newMap
         _ -> Map.insert parent_id newParentLst newMap
 -}
 
