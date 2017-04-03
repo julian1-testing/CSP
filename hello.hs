57f557e560f283f618f74fb55bbf72c207945b4f
julian1 <julian.atkinson71@gmail.com>
2017-04-04 09:25:50 +1000

    ok generating some nodes

--- hello.hs
+++ hello.hs
@@ -1,6 +1,17 @@
 
 import Text.XML.HXT.Core
 
+-- use selem for single element
+
+-- what we want is arg
+
+-- fuck.
+
+genNode :: ArrowXml a => String -> a XmlTree XmlTree
+genNode x =  
+  mkelem "div" []
+     [ txt x ]
+
 
 helloWorld  :: ArrowXml a => a XmlTree XmlTree
 helloWorld
@@ -9,17 +20,22 @@ helloWorld
         [ mkelem "title" []
           [ txt "Hello World" ]     -- (2)
         ]
-      , mkelem "body"
-        [ sattr "class" "haskell" ] -- (3)
+      , mkelem "body" [ sattr "class" "haskell" ] -- (3)
         [ mkelem "h1" []
           [ txt "Hello World" ]     -- (4)
         ]
+
+      <+> genNode "hi" 
+      <+> genNode "there" 
   ]
 
 
 
+-- it might be easier to just use text...
+-- 
+
 application = do
- root [] [helloWorld]                         -- (1)
+  root [] [helloWorld]                         -- (1)
       >>>
       writeDocument [withIndent yes] "hello.xml"   -- (2)
        
