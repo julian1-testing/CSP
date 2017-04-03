c967e831cb8776ad15fc89de50aa80007a58ad7c
julian1 <julian.atkinson71@gmail.com>
2017-04-03 10:18:22 +1000

    tidy query

--- test.sql
+++ test.sql
@@ -1,6 +1,7 @@
 
 begin;
 
+/*
 drop table if exists data;
 
 create table data (
@@ -20,22 +21,33 @@ INSERT INTO data values ( 6, 4, 'Red Rose', 12 );
 INSERT INTO data values ( 7, 3, 'Television', 100 );
 INSERT INTO data values ( 8, 3, 'Radio', 70 );
 INSERT INTO data values ( 9, 8, 'Webradio', 90 );
-
-
+*/
+-- 
 -- SQL Statement
 
 WITH recursive ChildrenCTE AS (
-  SELECT  id as Rootid , id
+  SELECT  
+    id as Rootid, 
+    id
   FROM    facet_count_view2
   UNION ALL
-  SELECT  cte.Rootid, d.id
+  SELECT  
+    cte.Rootid, 
+    d.id
   FROM    ChildrenCTE cte
           INNER JOIN facet_count_view2 d ON d.parent_id = cte.id
 )
-SELECT  d.id, d.parent_id, d.label, d.count, cnt.Children
+SELECT  
+  d.id, 
+  d.parent_id, 
+  d.label, 
+  d.count, 
+  cnt.Children
 FROM    facet_count_view2 d
         INNER JOIN (
-          SELECT  Rootid as id , COUNT(*) - 1 as Children 
+          SELECT  
+            Rootid as id , 
+            COUNT(*) - 1 as Children 
           FROM    ChildrenCTE
           GROUP BY Rootid
         ) cnt ON cnt.id = d.id
