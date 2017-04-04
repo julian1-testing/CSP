b08278e6d94184104bb21fef61581a2e8dc1145d
julian1 <julian.atkinson71@gmail.com>
2017-04-04 11:38:22 +1000

    update

--- Main.hs
+++ Main.hs
@@ -4,18 +4,17 @@
 import qualified Facet as F(getConceptNesting,main)
 import qualified FacetFormat as FF(main)
 
-import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple(connectPostgreSQL)
 
 main :: IO ()
 main = do
 
   conn <- connectPostgreSQL "host='postgres.localnet' dbname='harvest' user='harvest' sslmode='require'"
-
   -- change to getNestingFromDB
-  nestings <- F.getConceptNesting conn
+  -- nestings <- F.getConceptNesting conn
   -- facetList <- F.getFacetList conn
 
 
   -- nice!!!
-  -- F.main
-  FF.main
+  F.main
+  -- FF.main
