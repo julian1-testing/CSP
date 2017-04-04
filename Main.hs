ba1c5673d1214b9cc90f8973e169ddae567c092c
julian1 <julian.atkinson71@gmail.com>
2017-04-05 09:32:12 +1000

    tidying

--- Main.hs
+++ Main.hs
@@ -26,29 +26,32 @@ main = do
 
 
   -----------------------
-  -- get stuff for calculating facet counts
-  nestings <- Facet.getConceptNesting conn -- is this a fast lookup, should we move this out of the facet code...
-  facetList <- Facet.getFacetList conn
+  -- get stuff parent/child nestings
+  -- is this a fast lookup, should we move this out of the facet code...
+  nestings <- Facet.getConceptNesting conn 
+  -- mapM print nestings
+
+  facetLeafCounts <- Facet.getFacetList conn
+  -- mapM print facetLeafCounts
+
+
 
   -- compute facet counts
-  let m = Facet.buildLeafFacetMap facetList
+  let facetCounts = Facet.buildLeafFacetMap facetLeafCounts
         & Facet.propagateAllRecordsToRoot nestings 
+  -- (mapM print).(Map.toList) $ facetCounts
 
 
   -- get the concept, parents and labels from db as a Map
   let makePair (concept, parent, label) = (concept, (parent, label))  -- turn into key,val pairs needed for map,
   labels <- Facet.getConceptLabels conn >>= return.(Map.fromList).(map makePair)
-
-
-
-  -- print "################## labels map"
   -- (mapM print).(Map.toList) $ labels
 
 
-  -- join the facet counts with the parent and label details, in a form suitable for output formatting... 
-  let m' = 
-       -- Map.foldlWithKey f Map.empty m
-       Map.foldlWithKey f [] m
+  -- create a new list by zipping with label, and parent_id, and removing the root node
+  -- in a form suitable for the output formatting
+  let completeFacetList = 
+       Map.foldlWithKey f [] facetCounts
         where
         f m concept (count, records) = case concept of 
           -- ignore the root node
@@ -57,27 +60,17 @@ main = do
           Just concept_id ->
                 let (parent, label) = mapGet labels concept_id in
                 (concept_id, parent, label, count) : m
---                Map.insert (concept_id) (parent, label, count) m
+
+  -- (mapM print) completeFacetList
 
 
-  -- no - the key is the parent --- ugghhhhh ... how does 
-  -- no it's a list....
+  -- build the graph for output formatting 
+  let facetGraph = FacetFormat.buildFacetGraph completeFacetList
+  -- (mapM print).(Map.toList) $ facetGraph
 
-
-  -- let rootNode = (Nothing, "dummy", -999  )
-  -- recurse m (parent_id, label, count) depth = do
-
-
-  print "################## everything map"
-
-  -- (mapM print).(Map.toList) $ m'
-  (mapM print) $ m'
-
-  -- this thing takes a list....
-  let fm = FacetFormat.buildFacetGraph m'
  
-  -- what data structure does this need? 
-  FacetFormat.printXMLFacetGraph fm 
+  -- format the thing -
+  FacetFormat.printXMLFacetGraph facetGraph
  
   return () 
 
@@ -86,95 +79,4 @@ main = do
 
 
 
-
-{-
-  -- actually we should transform it into the subset of whatever is the most convent
-
-          id as concept_id, 
-          parent_id,
-          label ,
-          -999 -- count  -- dummy value
--} 
-
-  -- if we are going to desctructure this...  we'll have to match...
-
-  -- let nestingsMap = Map.fromList nestings
-  -- (Map.toList nestingsMap) & mapM print  
-
-  -- WE DON"T WANT map - because we don't want to have to deal with the parent node... for printing... 
-  -- or do we... 
-  -- no i 
-
-  -- map means we can't drop the element...
-
-  -- So just do a fold - to reorganise it again... into a list without the parent....
-  -- GOOOD
-
-  -- map will preserve the number of elements which isn't much good.
-  -- here we add in the parent_id
-
-  -- it has to be a fold or filter to remove the Nothing....
-
-{-   
-  let m' = Map.mapWithKey f m 
-        where
-        f concept (count, _) = case concept of
-          Nothing ->         (count,  Nothing,  1234)
-          Just concept_id -> (count, mapGet nestingsMap concept_id,  1234)
-
-  -- so we just need to tack in the parent and the label... again...
--}
-  -- IMPORTANT - should we move the db code outside of the facet stuff...
-  -- if the nestings were ''
-
-  -- mapM print nestings 
-  -- it might easier just to take the Facet structure - and add in the parent_id, and labels?
-  -- it's also already a map....
-
-{-
-  facetList_ <- FacetFormat.getFacetList conn
-
-  -- this is a bit messy
-  -- let  facetList' = map (\f (a,b,c, d) -> (a,b,c, 123) )  facetList
-
-  let g = FacetFormat.buildFacetGraph facetList_
-
-  FacetFormat.printXMLFacetGraph g
--}
-
-
-
-{-
-  VERY IMPORTANT - we must build the list out of our returned values. 
-    because that is the result of our query
-      - we just need to add in the nestings 
-      - and the labels.
-
-  - and it's already in a nice list...
-  - except we have to do it from a list
-
-  - actually it's not quite as simple
-
-  -- it's just a freaking join...
-  -- but I think we want to use the actual counts...
--}
-
-  -- but we have to structure it in terms of 
-  -- it's already a fast lookup - but we just don't have the children....
-
-  -- I think the only thing we need is to add in the parent and label and we're done.
-
-  -- print "################## nestings list"
-  -- mapM print nestings
-
---  print "################## labels list "
-
-  -- TODO change name nestings to parents, or parentNestings or something? or broader ?
-
-
-{-
-  labels <- Facet.getConceptLabels conn 
-  mapM print labels
-  let labels' =  Map.fromList . map (\(k, a, b) -> (k, (a,b))) $   labels
--}
 
