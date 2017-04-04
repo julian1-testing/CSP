363e23ff432d63d305e8bedef05e48dfac33f2e2
julian1 <julian.atkinson71@gmail.com>
2017-04-04 11:01:36 +1000

    update build script

--- build.sh
+++ build.sh
@@ -5,17 +5,16 @@
 FLAGS="-O2 -outputdir output"
 
 ############
-rm output -rf
-ghc $FLAGS harvest.hs || exit
+
+# rm output -rf
+# ghc $FLAGS harvest.hs || exit
+
+# rm output -rf
+# ghc $FLAGS  load-scheme.hs || exit
 
 
-rm output -rf
-ghc $FLAGS  load-scheme.hs || exit
-
-rm output -rf
-ghc $FLAGS recurse.hs || exit
-
-rm output -rf
-ghc $FLAGS recurse2.hs || exit
+# we don't want to remove the files in output otherwise we have to build everythign again
+rm Main
+ghc $FLAGS Facet.hs Main.hs FacetFormat.hs
 
 
