c4bd6a2153b944bada2973cf26fa5a11e5744277
julian1 <julian.atkinson71@gmail.com>
2017-04-07 08:16:45 +1000

    add utf

---
+++ examples/Utf.hs
@@ -0,0 +1,26 @@
+
+-- http://stackoverflow.com/questions/2086842/using-haskell-to-output-a-utf-8-encoded-bytestring
+
+-- {-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
+
+import qualified Data.ByteString as B
+
+-- https://hackage.haskell.org/package/bytestring-0.10.8.1/docs/Data-ByteString.html
+-- cons  to append a single character
+
+main =  do
+
+  -- this works by itself, it's using Data.Text.Text
+  let a = T.pack "čušpajž日本語"  
+  let b = T.pack " whoot日本語"  
+
+
+  -- let y = B.append a a 
+  -- T.putStrLn $ "čušpajž日本語" ++ y
+
+  T.putStrLn $ T.append a  b
+
+
