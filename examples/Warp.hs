02bc78d19a4264b6547204253294b2e9be397388
julian1 <julian.atkinson71@gmail.com>
2017-04-07 08:02:29 +1000

    have problem

--- examples/Warp.hs
+++ examples/Warp.hs
@@ -5,6 +5,13 @@ import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (status200)
 import Network.HTTP.Types.Header (hContentType)
 
+
+import qualified Data.Text    as T
+import qualified Data.Text.IO as T
+import qualified Data.Text.Encoding as E
+
+
+
 main = do
     let port = 3000
     putStrLn $ "Listening on port " ++ show port
@@ -17,7 +24,21 @@ main = do
 app :: Application
 app req f = do
     print "got request"
-    x <- f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!" 
+
+    -- this works by itself, it's using Data.Text.Text
+    let a = T.pack "čušpajž日本語"  
+    let b = T.pack " whoot日本語"  
+    let c = T.append a b
+
+    -- 
+    let d = E.encodeUtf8 c
+
+
+    -- responseLBS - expects a Lazy Byte String - while we have
+
+    -- x <- f $ responseLBS status200 [(hContentType, "text/plain")] d --"Hello world!" 
+    x <- f $ responseLBS status200 [(hContentType, "text/plain")] d --"Hello world!" 
+
     print "done request"
     return x
 
