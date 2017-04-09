28d1511ab4b824aae5b0acaa166ca680d22998bd
julian1 <julian.atkinson71@gmail.com>
2017-04-09 14:20:23 +1000

    tidy build.sh

--- build.sh
+++ build.sh
@@ -29,32 +29,5 @@ ghc $FLAGS -main-is FacetRequest.main ./src/FacetRequest.hs -o target/FacetReque
 ############
 
 
-
-
 #### OLD
-
-# ghc $FLAGS -main-is Harvest.main src/MetadataRecord.hs src/Harvest.hs src/CSW.hs src/Helpers.hs -o target/Harvest 
-
-# rm output -rf
-# ghc $FLAGS  load-scheme.hs || exit
-
-# record parsing
-# ghc -main-is MetadataRecord.main -outputdir tmp src/MetadataRecord.hs src/Helpers.hs -o ./target/MetadataRecord
-
-# ghc -main-is RecordStore.main -outputdir tmp src/RecordStore.hs src/MetadataRecord.hs src/Helpers.hs -o ./target/RecordStore
-
-# -- ghc -package ghc -v -outputdir ../tmp RecordStore.hs 
-# ghc  -main-is RecordStore.main --outputdir ../tmp RecordStore.hs 
-
-#####
-# importnat - it will automatically find all the files  
-## pushd ./src
-##ghc -main-is RecordStore.main -outputdir ../tmp RecordStore.hs  -o ../target/RecordStore
-
-
-# compile Facet test code,
-# ghc -main-is Facet.main -outputdir tmp Facet.hs  -o ./Facet
-
-# Format only...
-# ghc -main-is Facet.main -outputdir tmp  FacetFormat.hs  -o ./FacetFormat
 
